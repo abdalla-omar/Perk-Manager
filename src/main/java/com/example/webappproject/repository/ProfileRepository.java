822100a0e387de5fd3125d1f3b0bed1cb29d4c91
Abdalla-00 <abdalla.m.a.omar@gmail.com>
2025-11-02 21:41:47 -0500

    update

--- src/main/java/com/example/webappproject/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webappproject.repository;
-
-import com.example.webappproject.model.Profile;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfileRepository extends CrudRepository<Profile, Long> {
-    Profile findById(long id);
-}
--- src/main/java/com/example/webappproject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webappproject.repository;
-
-import com.example.webappproject.model.AppUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<AppUser, Long> {
-    AppUser findById(long id);
-    AppUser findByEmail(String email);
-}
