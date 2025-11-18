77f976ebd8e6339ae4ef3351cd6155fb4aba3ba7
Abdalla Omar <abdalla.m.a.omar@gmail.com>
2025-11-18 18:50:14 -0500

    Cleanup 2

--- src/main/java/com/example/perkmanager/query/GetAllPerksQuery.java
+++ src/main/java/com/example/perkmanager/query/GetAllPerksQuery.java
@@ -1,19 +1,9 @@
 package com.example.perkmanager.query;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Query: Get all perks
  * Read-only operation to retrieve all perks from read model
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class GetAllPerksQuery {
     // No parameters needed for getting all perks
-    // Can be extended with pagination later
-    private Integer page;
-    private Integer size;
 }
